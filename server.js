69a721712006d19f3f8068c8466f8610c6faafe2
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-18 08:23:27 -0300

    initial setup

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const PORT = 3000;
+
+http.listen(PORT, () => console.log('listening', PORT));
