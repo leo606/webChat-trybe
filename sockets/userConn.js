38a33d725d938a92720bf28bdd3d5de8320a997d
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-01-19 08:58:49 -0300

    remove console log

--- sockets/userConn.js
+++ sockets/userConn.js
@@ -3,7 +3,6 @@ const genToken = require('../utils/genToken');
 module.exports = (io, socket, connectedUsers) => {
     const nick = genToken(16);
     socket.emit('nick', nick);
-    console.log(connectedUsers);
 
     connectedUsers.push({ nick, sId: socket.id });
     io.emit('usersList', connectedUsers.map((user) => user.nick));
